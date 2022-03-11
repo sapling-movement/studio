d12db19f348f5b5b622dbe99c15ad7a9b3d76305
timonforrer <timon.forrer@gmail.com>
2022-03-11 22:55:49 +0100

    Add schema for navigation

---
+++ schemas/documents/navigation.js
@@ -0,0 +1,29 @@
+export default {
+  name: 'navigation',
+  title: 'Navigation',
+  type: 'document',
+  i18n: true,
+  fields: [
+    {
+      name: 'items',
+      title: 'Items',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{ type: 'modularPage' }],
+          options: {
+            filter: ({ document }) => {
+              return {
+                filter: '__lang == $lang',
+                params: {
+                  lang: document.__lang
+                }
+              }
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
