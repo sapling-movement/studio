45e4b74734c51f538b9af608e69c851944c49c15
timonforrer <timon.forrer@gmail.com>
2022-03-11 22:57:06 +0100

    Hook up all new schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -2,18 +2,28 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-import pageBase from './pageBase'
-import imageWithCaption from './imageWithCaption'
-import navigation from './navigation'
+import articleList from './objects/articleList'
+import pageBase from './objects/pageBase'
+import imageWithCaption from './objects/imageWithCaption'
+import portableText from './objects/portableText'
+import translation from './objects/translation'
+
+import navigation from './documents/navigation'
+import uiTranslations from './documents/uiTranslations'
+
 import modularPage from './pageTypes/modularPage'
 import blogPost from './pageTypes/blogPost'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
+    articleList,
     pageBase,
     imageWithCaption,
+    portableText,
+    translation,
     navigation,
+    uiTranslations,
     modularPage,
     blogPost
   ]),
