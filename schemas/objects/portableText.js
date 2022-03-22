be2542a57650d12b0a21653ba2ae2e73257d6047
timonforrer <timon.forrer@gmail.com>
2022-03-22 23:39:29 +0100

    Add annotations for internal links

--- schemas/objects/portableText.js
+++ schemas/objects/portableText.js
@@ -1,3 +1,5 @@
+import { FiLink, FiExternalLink } from 'react-icons/fi'
+
 export default {
   name: 'portableText',
   title: 'Portable text',
@@ -8,7 +10,57 @@ export default {
       title: 'Text',
       type: 'array',
       of: [
-        { type: 'block' },
+        {
+          type: 'block',
+          marks: {
+            annotations: [
+              {
+                title: 'Internal link',
+                name: 'internalLink',
+                type: 'object',
+                blockEditor: {
+                  icon: FiLink
+                },
+                fields: [
+                  {
+                    name: 'reference',
+                    type: 'reference',
+                    to: [
+                      { type: 'modularPage' },
+                      { type: 'blogPost' }
+                    ],
+                    options: {
+                      filter: ({ document }) => {
+                        return {
+                          filter: '__lang == $lang',
+                          params: {
+                            lang: document.__lang
+                          }
+                        }
+                      }
+                    }
+                  }
+                ]
+              },
+              {
+                title: 'External link',
+                name: 'externalLink',
+                type: 'object',
+                blockEditor: {
+                  icon: FiExternalLink
+                },
+                fields: [
+                  {
+                    title: 'URL',
+                    name: 'href',
+                    type: 'url',
+                    validation: Rule => Rule.uri({allowRelative: false, scheme: ['https', 'http', 'mailto', 'tel']})
+                  },
+                ]
+              }
+            ]
+          }
+        },
         { type: 'imageWithCaption' }
       ],
     },
